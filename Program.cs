8b5e87a90508947c2666b5b38a056c5c63f35feb
Alex Pierides <alexander.pierides@founddigital.com.au>
2018-07-17 14:18:36 +1000

    updates

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,12 +13,17 @@ namespace CheckWebSSL
     class Program
     {
         static List<Site> websites;
+        static int ct = 1;
         static void Main(string[] args)
         {
-            try {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;//| SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
 
-                Task.WaitAll(CheckSites());
-                Console.ReadKey();
+            try
+            {
+               Console.WriteLine(ServicePointManager.SecurityProtocol);
+               CheckSites().Wait(300000);
+               WriteResults();
+              Console.ReadKey(); 
             }
             catch(Exception ex)
             {
@@ -25,6 +31,21 @@ namespace CheckWebSSL
                 Console.ReadKey();
             }
         }
+
+     
+
+        private static void WriteResults()
+        {
+            using (var writer = new StreamWriter(@"D:\Clients\Sensis\SensisURLSResults.CSV"))
+            {
+                websites.ForEach(web =>
+                {
+                    writer.WriteLine(web.URL + "," + web.Message + "," + web.InteriorMessage+',' + web.IsEncrypted + "," + web.Status);
+                });
+              
+            }
+        }
+
         private static List<Site> GetUrls()
         {
             var sites = new List<Site>();
@@ -42,34 +63,92 @@ namespace CheckWebSSL
         {
             public string URL { get; set; }
             public bool IsEncrypted { get; set; }
+            public string Message { get; set; }
+            public string Status { get; set; }
+            public string InteriorMessage { get; set; }
+
         }
 
         private static async Task CheckSites()
         {
             websites = GetUrls();
-            foreach (var website in websites)
-              await CheckSite(website);
-            
+            //websites = new List<Site>()
+            //{
+            //    new Site()
+            //    {
+            //        URL = "https://www.advancedheavyvehicles.com.au"
+            //    },
+            //          new Site()
+            //    {
+            //        URL = "https://inbox.google.com"
+            //    },
+            //          new Site()
+            //    {
+            //        URL = "https://stackoverflow.com"
+            //    },
+            //          new Site()
+            //    {
+            //        URL = "https://www.theage.com.au/"
+            //    },new Site()
+            //    {
+            //        URL = "https://tipping.afl.com.au"
+            //    },
+            //          new Site()
+            //    {
+            //        URL = "https://www.advancedheavyvehicles.com.au"
+            //    }
+            //};
+
+            Task.WhenAll(websites.Select(x =>  CheckSite(x)));
+         
+             
         }
 
         private static async Task CheckSite(Site web)
         {
             try
             {
-                var request = WebRequest.Create(web.URL);
-                //   request.UserAgent = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36";
+                Console.Clear();
+                Console.WriteLine(ct);
+                ct++;
+                if (web.URL == "https:")
+                {
+                    web.IsEncrypted = false;
+                    web.Status = "No Site";
+                    return;
+                }
+                // File.WriteAllText("sample.txt", new HttpClient().GetStringAsync("https://www.howsmyssl.com").Result);
+                var request = (HttpWebRequest)WebRequest.Create(web.URL);
+                request.Method = "GET";
+                request.Accept = "text/html";
+                request.UseDefaultCredentials = true;
+                request.CookieContainer = new CookieContainer();
+                request.Accept = @"text/html, application/xhtml+xml, */*";
+                request.Referer = @"http://www.yellow.com/";
+                request.Headers.Add("Accept-Language", "en-GB");
+                request.UserAgent = @"Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)";
+                //request.Host = @"www.yellow.com/";
 
-                Console.WriteLine(request.Method);
+              //  request.Headers.Add("UserAgent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36");
                 var response = (HttpWebResponse)await Task.Factory
-                    .FromAsync<WebResponse>(request.BeginGetResponse,
-                                            request.EndGetResponse,
-                                            null);
-                web.IsEncrypted = true;
+                .FromAsync<WebResponse>(request.BeginGetResponse,
+                                        request.EndGetResponse,
+                                        null);
+                
+                web.IsEncrypted = response.StatusCode.ToString() == "OK";
+                web.Status = response.StatusCode.ToString();
+                web.Message = response.StatusDescription;
             }
             catch(WebException ex)
             {
                 web.IsEncrypted = false;
-                Console.WriteLine(ex.Status);
+               
+             
+             //   Console.WriteLine(web.URL + " " + ex.Message);
+                web.Status = ex.Status.ToString();
+                web.Message = ex.Message;
+                web.InteriorMessage = ex.InnerException != null ? ex.Message : "";
+
             }
           
         }
